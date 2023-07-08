9686b0b82f6285aa39c9a4062d901e3cf71e4ccf
Alejandro Ibañez <daibanez11@gmail.com>
2023-07-08 12:10:15 -0500

    ultimo save

--- ProyectoCajero/script.js
+++ ProyectoCajero/script.js
@@ -3,15 +3,15 @@ let cuentas = [
   { nombre: "Gera", saldo: 290, password: "ee89"}, 
   { nombre: "Maui", saldo: 67, password: "91pt" } 
 ];
+const maximo = 990;
+const minimo = 10;
 
 document.querySelector("#persona").addEventListener("change",leerPersona)
 document.getElementById("menu").style.display = "none"
 
 function leerPersona(){
-  //document.getElementById("persona").selectedIndex
   let indice = parseInt(document.querySelector("#persona").value);
   let nombre = cuentas[indice].nombre;
-  let saldo = cuentas[indice].saldo;
 
   document.getElementById("saludo").innerHTML = "Hola," + nombre;
   document.getElementById("meter-password").style.display = "block"
@@ -20,8 +20,6 @@ function leerPersona(){
 
 function ingreso(){
   let indice = parseInt(document.querySelector("#persona").value);
-  let nombre = cuentas[indice].nombre;
-  let saldo = cuentas[indice].saldo;
   let recibida = document.getElementById("password").value;
   let contra = cuentas[indice].password;
 
@@ -44,13 +42,15 @@ function agregar(e){
   let indice = parseInt(document.querySelector("#persona").value);
   let saldo = cuentas[indice].saldo;
   let ingresoMonto = Number(document.querySelector("#ingresoMonto").value);
-  document.querySelector("#montoIngresado").innerHTML = "Tu monto ingresado es: " + ingresoMonto;    
+  document.querySelector("#montoIngresado").innerHTML = "Tu monto ingresado es: " + ingresoMonto;   
   let totalSumado = (saldo += ingresoMonto)
-  cuentas[indice].saldo = totalSumado;
-  document.querySelector("#nuevoSaldo").innerHTML = "Tu monto total es: " + totalSumado;
-  if (saldo >= 990) {
-    alert("No puedes ingresar mas dinero, el limite maximo en al cuenta es $990 y en este momento tienes: "+ saldo)
-}
+    if (totalSumado <= maximo) {
+      cuentas[indice].saldo = totalSumado;
+       document.querySelector("#nuevoSaldo").innerHTML = "Tu monto total es: " + saldo; {    
+    }
+      }else{
+        alert("No puedes ingresar mas dinero, el limite maximo en al cuenta es $990 y en este momento tienes: "+ saldo)
+      }  
 }
 
 document.getElementById("fRetirar").addEventListener("submit",retiro)
@@ -61,11 +61,12 @@ function retiro(e){
   let saldoRetirado =  Number(document.querySelector("#ingresoMontoR").value);
   document.querySelector("#saldoRetirado").innerHTML = "Tu monto retirado es: " + saldoRetirado;
   let nuevoSaldo = saldo - saldoRetirado;
-  cuentas[indice].saldo = nuevoSaldo;    
-  document.querySelector("#nuevoSaldoR").innerHTML = "Tu monto total despues de retirar es es: " + nuevoSaldo; 
-  if (nuevoSaldo <= 10) {
+  if (nuevoSaldo >= minimo) {
+    cuentas[indice].saldo = nuevoSaldo;    
+  document.querySelector("#nuevoSaldoR").innerHTML = "Tu monto total despues de retirar es: " + nuevoSaldo;     
+  } else {
     alert("No puedes retirar mas dinero, el limite minimo en al cuenta es $10 y en este momento tienes: "+ saldo)
-}
+  }
 }
 
 function consulta(){
